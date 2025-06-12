ba5c2dcdc79255a95c1eeca2232acdd7996b86a0
Parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-06-12 11:32:15 +0330

    feat: reintroduce corrected and incomplete header for Map class

--- include/Map.hpp
+++ include/Map.hpp
@@ -1,28 +1,38 @@
-#ifndef MAP_HPP
-#define MAP_HPP
+// Map.h
+#ifndef MAP_H
+#define MAP_H
 
+#include <string>
 #include <vector>
-#include <iostream>
-#include "Position.hpp"
-#include "Item.hpp"
+#include <unordered_map>
+#include "Location.hpp"
 
 class Map {
-
-    private:
-
-        int width;
-        int height;
-        std::vector<std::vector<char>> grid;
-
-
-    public:
+private:
+    // std::string namePlayer;
+    std::unordered_map<std::string, Location*> locations;
+    std::unordered_map<std::string, Location*> playerPositions;
+    void addLoation(const std::string&);
+    void connectLocaiton(const std::string&, const std::string&);
+public:
     
-        Map(int w = 10, int h = 10); 
-        void clear();
-        void placeItem(const Item& item);
-        void placeChar(const Position& pos, char symbol);
-        void display() const;
-        bool isValidPosition(const Position& pos) const;
+    Map();
+    Map(const Map&) = delete;
+    Map operator =(const Map&) = delete;
+
+    static Map& getInstance();
+
+    void totalLocation();
+    void negiborLocation();
+    void getPlayersLocation(const std::string&);
+    std::string findShortestPath(const std::string&, const std::string&);
+    
+    void setPlayerPosition(const std::string&, const std::string&);
+    Location* getPlayerPosition(const std::string&) const;
+
+    void printNeighbors();
+    void print();
+    void printPlayers() const;
 
 };
 
