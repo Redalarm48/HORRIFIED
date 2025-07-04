56cca352add32d33b4ecf532172640a29fda39ba
Parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-07-04 10:45:07 +0330

    feat: extend Heroes class with perk card system, item management, and enhanced action control

--- include/Hero.hpp
+++ include/Hero.hpp
@@ -6,38 +6,51 @@
 #include "Map.hpp"
 #include <string>
 // #include "Villagers.hpp"
+enum class itemType;
 enum class Status{
     Alive,
     Dade
 };
 
+class Map;
+class Item;
+enum class itemType;
+
 class Heroes {
 private:
+
     std::string nameHero;
-    Status statusHero;
     PerkDeck perkCard;
+    Status statusHero;
     Map& locationHero;
     int numberActionTaken;
     int maxActions;
     std::vector<Item*> inventory;
+    std::vector<PerkCard> perkCards;
 
+    
+    static std::unordered_map<std::string, bool> invisibleItemCollected;
+    static std::unordered_map<std::string, bool> coffinDestroyed;
 
+    
 public:
     Heroes(int,const std::string, Map&);
     virtual ~Heroes() = default;
 
     std::string getName() const;
     Status getStatus() const;
+    std::vector<PerkCard> getInGamePerkCards();
     void setStatus(Status);
+    void setLocation(const std::string&, const std::string&);
 
     Location* getLocationHeroPtr() const;
     std::string getLocationHero() const;
     bool canTakeAction() const;
     void resetActions();
 
-    void move();
+    void move(bool = false);
     void guide();
-    void pickUp(Item* item);
+    void pickUp(Item*);
     void Advance();
     void defeat();
     // virtual void specialAction() = 0;
@@ -45,21 +58,35 @@ public:
     void showInventory() const;
     void removeFromInventory(Item* item);
     std::vector<Item*> getInventory();
+    bool removeItems(itemType type, int count);
+    bool hasItems(itemType type, int count) const;
+    void addPerkCard(const PerkCard& card);
+    void usePerkCard(int index);
+    void showPerkCards() const;
+    const std::vector<PerkCard>& getPerkCards() const;
+    void decreaseAction();
+    void removePerkCard(int index);
 
 
-protected:
+    static std::unordered_map<std::string, bool>& getInvisibleItemCollected();
+    static std::unordered_map<std::string, bool>& getcoffinDestroyed();
+
+    void handlePickUp( Map& map);
+
+    void setNumberActionTaken(int num);
     void incrementAction();
+
 };
 
 class Archaeologist : public Heroes {
 public:
-    // Archaeologist(Map&);
+    Archaeologist(Map&);
     // void specialAction() override;
 };
 
 class Mayor : public Heroes {
 public:
-    // Mayor(Map&);
+    Mayor(Map&);
     // void specialAction() override;
 };
 
