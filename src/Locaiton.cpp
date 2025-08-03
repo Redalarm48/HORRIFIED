83a07f5815fc724e333331d8f50910c275cda3a6
Parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-08-03 16:00:11 +0330

    Refactor Location.cpp: migrate to typed entity system
    Replaced string-based player tracking with separate lists for heroes, monsters, items, and villagers using enum types. Added validation and structured add/remove methods.

--- src/Locaiton.cpp
+++ src/Locaiton.cpp
@@ -1,36 +1,81 @@
-#include "Location.hpp"
-
-Location::Location(const std::string& nameLocaiton) {
+#include "../include/Location.hpp"
+#include "../include/Item.hpp"
+Location::Location(const NameLocation nameLocaiton) {
     this->nameLocation = nameLocaiton;
 }
 
-const std::string& Location::getName() const {
-    return nameLocation;
+const NameLocation Location::getNameLocation() const {
+    return this->nameLocation;
+}
+const std::vector<NameHeroes> Location::getNameHeroes() const {
+    return this->heroes;
+}
+const std::vector<NameMonster> Location::getNameMonsters() const {
+    return this->monsters;
+}
+const std::vector<NameItem> Location::getNameItems() const {
+    return this->items;
+}
+const std::vector<NameVillagers> Location::getNameVillagers() const {
+    return this->villagers;
+}
+const std::vector<Location*>& Location::getNeighbors() const {
+    return neighbors;
 }
 
 void Location::addNeighbor(Location* neighbor) {
     neighbors.push_back(neighbor);
 }
-
-const std::vector<Location*>& Location::getNeighbors() const {
-    return neighbors;
-}
-std::ostream& operator<<(std::ostream& os, const Location& loc) {
-    os << "Location: " << loc.getName(); // یا هر اطلاعات دیگه‌ای که خواستی
-    return os;
-}
-void Location::addPlayer(const std::string& name) {
-    players.push_back(name);
+void Location::addHeroes(const NameHeroes& nameHeroes) {
+    heroes.push_back(nameHeroes);
 }
 
-void Location::removePlayer(const std::string& name) {
-    auto it = std::find(players.begin(), players.end(), name);
-    if (it != players.end()) {
-        players.erase(it);
+void Location::addMonsters(const NameMonster& nameMonsters) {
+    monsters.push_back(nameMonsters);
+}
+void Location::addItem(const NameItem& nameItems) {
+    if(std::find(items.begin(), items.end(), nameItems) == items.end()) {
+        items.push_back(nameItems);
+    }
+
+}
+void Location::addVillagers(const NameVillagers& nameVillagers) {
+    villagers.push_back(nameVillagers);
+}
+
+void Location::removeHeroes(const NameHeroes& nameHeroes) {
+    auto chek = std::find(heroes.begin(), heroes.end(), nameHeroes);
+    if (chek != heroes.end()) {
+        heroes.erase(chek);
+    }
+    else {
+        throw std::invalid_argument("The hero you are looking for could not be found");
     }
 }
-
-std::vector<std::string> Location::getPlayers() const {
-    return players;
+void Location::removeMonsters(const NameMonster& nameMonsters) {
+    auto chek = std::find(monsters.begin(), monsters.end(), nameMonsters);
+    if(chek != monsters.end()) {
+        monsters.erase(chek);
+    }
+    else {
+        throw std::invalid_argument("The monster you are looking for could not be found");
+    }
 }
-
+void Location::removeItem(const NameItem& nameItem) {
+    auto chek = std::find(items.begin(), items.end(), nameItem);
+    if(chek != items.end()) {
+        items.erase(chek);
+    }
+    else {
+        throw std::invalid_argument("The item you are looking for could not be found");
+    }
+}
+void Location::removeVillagers(const NameVillagers& nameVillagers) {
+    auto chek = std::find(villagers.begin(), villagers.end(), nameVillagers);
+    if(chek != villagers.end()) {
+        villagers.erase(chek);
+    }
+    else {
+        throw std::invalid_argument("The villager you are looking for could not be found");
+    }
+}
