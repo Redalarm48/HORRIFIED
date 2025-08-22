93b81cfcbe8576e6aeb13d6bdf516938a106e710
Parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-08-22 23:22:40 +0330

    Complete Enemy/Monster classes with movement, actions, and interaction logic

---
+++ include/NameEnum.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+enum class NameHeroes {
+    MAYOR, ARCHAEOLOGIST,
+    Default
+};
+
+enum class NameItem {
+    FLOWER, TAROT_DECK, GARLIC, MIRRORED_BOX, STAKE,
+    SCROLL_OF_THOTH, VIOLIN, TABLET, WOLFSBANE, CHARM,
+    DART, FIRE_POKER, RAPIER, SHOVEL, TORCH, PITCHFORK,
+    RIFLE, SILVER_CANE, KNIFE, PISTOL, BEAR_TRAP, SPEARGUN,
+    ANATOMY_TEXT, CENTRIFUGE, KITE, RESARCH, TELESCOPE,
+    SEARCHLIGHT, EXPERIMENT, ANALYSIS, ROTENONE, COSMIC_RAY_DIFFUSER,
+    NEBULARLUM, MONOCANE_MIXTURE, FOSSIL,
+    Default
+};
+
+enum class NameLocation{
+    CAVE, CAMP, PRECINCT, INN, BARN,
+    DUNGEON, TOWER, MUSEUM, CRYPT,
+    ABBEY, MANSION, SHOP, DOCKS,
+    HOSPITAL, CHURCH, LABORATORY,
+    GRAVEYARD, INSTITUTE, THEATRE,
+    Default
+
+};
+
+
+enum class NameMonster {
+    DRACULA, INVISIBLE_MAN, FRENZI,
+    Default
+};
+
+
+enum class NameVillagers{
+    Dr_crunly, Maleva, 
+    Fritz, Maria, 
+    Prof_pearson, Dr_read,
+    WilnureAndChick,
+    Default
+};
+
+enum class NameAction {
+    MOVE, GUIDE, PICKUP,
+    DEFAULT
+};
