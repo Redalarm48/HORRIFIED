218dea8ba8f6cdd93ba8d60e283875a2644eb75c
Redalarm48 <moni.mohaghegh2@gmail.com>
2025-08-22 21:00:20 +0330

    InvisibleMan's movement has been replaced

--- include/InvisibleMan.hpp
+++ include/InvisibleMan.hpp
@@ -13,6 +13,6 @@ private:
 
 
         // void usePower(Heroes &h ,const std::vector<Heroes*>& heroes, const std::vector<std::pair<std::string, std::string>>& villagers) override;
-
+        bool moveMonster(Villager&,Heroes&, Heroes&, Heroes&, Heroes&, bool = false) override;
         // void move();
 };
--- src/InvisibleMan.cpp
+++ src/InvisibleMan.cpp
@@ -5,3 +5,47 @@
 InvisibleMan::InvisibleMan(Map& map) : Monster(NameMonster::INVISIBLE_MAN, map, NameLocation::GRAVEYARD){}
 
 
+bool InvisibleMan::moveMonster(Villager& villager, Heroes& hero1, Heroes& hero2, Heroes& hero3, Heroes& hero4, bool w) {
+   
+    std::vector<std::pair<NameLocation, int>> targets;
+   
+    for(auto& v : villager.getVillagers()) {
+        if(v.second.getLocationVillager() == getNameLocationMonster()) {
+            if(w && chek) {
+                v.second.removeVillager(v.second.getNameVillager());
+                this->chek = false;
+            }
+            return true;
+        } 
+            targets.emplace_back(v.second.getLocationVillager(), 0);
+    }
+
+    if(villager.getVillagers().empty()) {
+        std::cerr << "not found viilager";
+        return false;
+    }
+
+    int minDist = INT_MAX;
+
+    for(auto& [name, number] : targets) {
+        try {
+            number = getLocationMonster().findShortestPath<int>(getNameLocationMonster(), name);
+            if(number < minDist) {
+                minDist = number;
+            }
+        } catch(...) {
+        }
+    }
+    for(auto& [name, number] : targets) {
+        if(number == minDist) {
+            this->setMonsterPosition(getLocationMonster().findShortestPath<NameLocation>(getNameLocationMonster(), name));
+            if(this->getNameLocationMonster() == name) {
+                this->chek = true;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    throw std::invalid_argument("error move Invisible man");
+}
