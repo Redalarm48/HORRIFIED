1d82a45de9016c300b7bf397eb24aedeb6d22efb
Parsa12309 <parsa.rashtiyan.1007@gmail.com>
2025-08-22 23:15:02 +0330

    feat(Heroes): fully implement Heroes class including actions, movement, and perk handling

--- include/Hero.hpp
+++ include/Hero.hpp
@@ -25,17 +25,22 @@ private:
     int maxActions;
     Status statusHero;
 
+    std::vector<std::pair<NameItem, NameLocation>> nameItemPickUpInvisibleMan;
     std::vector<PerkDeck> perkCards;
     
 public:
-    Heroes(int,const NameHeroes, Map&, const NameLocation&);
+    Heroes(int,const NameHeroes, Map&, const NameLocation&, PerkDeck&);
     virtual ~Heroes() = default;
+
+
 
     std::string getName() const;
     NameHeroes getNameHero() const;
     std::string changNameHeroesTheString(const NameHeroes&);
-    Status getStatus() const;
     std::vector<PerkDeck> getInGamePerkCards();
+    std::vector<std::pair<NameItem, NameLocation>> getNameItemPickUpInvisibleMan() const;
+
+    void setPerkCard(const PerkDeck&);
 
     NameLocation getLocationHero() const;
     bool canTakeAction() const;
@@ -44,9 +49,10 @@ public:
     bool cheknumberDistance(const NameLocation&);
 
 
-    void move(Villager&,const NameLocation&, bool);
+    void move(Villager&,const NameLocation&, bool, const std::vector<NameVillagers>&);
     void guide(Villager&, const NameVillagers&);
     void pickUp(Item&, const std::vector<NameItem>&);
+    bool defieat(Monster&, Item&);
 
     int getNumberActionTaken() const;
 
@@ -61,11 +67,21 @@ public:
 
 class Archaeologist : public Heroes {
 public:
-    Archaeologist(Map&);
+    Archaeologist(Map&, PerkDeck&);
     void specialAction(Map&, const std::vector<Item*>& itemList);
 };
 
 class Mayor : public Heroes {
 public:
-    Mayor(Map&);
+    Mayor(Map&, PerkDeck&);
+};
+
+class Courier : public Heroes {
+public:
+    Courier(Map&, PerkDeck&);
+};
+
+class Scientist : public Heroes {
+public:
+    Scientist(Map&, PerkDeck&);
 };
--- src/Hero.cpp
+++ src/Hero.cpp
@@ -35,9 +35,9 @@ void Heroes::setHeroesPosition(const NameLocation& newNameLocationHeroes) {
     }
 }
 
-Heroes::Heroes(int maxAction, const NameHeroes nameHero, Map& locationHero, const NameLocation& newNameLocationHeroes)  : nameHero(nameHero), statusHero(Status::Alive), locationHero(locationHero),
+Heroes::Heroes(int maxAction, const NameHeroes nameHero, Map& locationHero, const NameLocation& newNameLocationHeroes, PerkDeck& perkDeck)  : nameHero(nameHero), statusHero(Status::Alive), locationHero(locationHero),
     numberActionTaken(0), maxActions(maxAction) {
-        // perkCard.drawCard();
+        this->setPerkCard(perkDeck.drawCard());
         auto chek = std::find_if(locationHero.map.begin(), locationHero.map.end(), [&newNameLocationHeroes](const auto& p) {
         return p.first == newNameLocationHeroes;
         });
@@ -80,7 +80,7 @@ int Heroes::getNumberActionTaken() const {
 
 
 
-void Heroes::move(Villager& villager,const NameLocation& newNameLocationHero, bool skipVillager) {
+void Heroes::move(Villager& villager,const NameLocation& newNameLocationHero, bool yesOrno, const std::vector<NameVillagers>& villagersMove) {
 
     auto neighbors = locationHero.getNeighborLocation(nameLocationHeroes);
     
@@ -97,11 +97,10 @@ void Heroes::move(Villager& villager,const NameLocation& newNameLocationHero, bo
     }
     
     
-    if(!skipVillager) {   
-        std::vector<NameVillagers> villagersMove;
-        villagersMove = villager.moveLocation(this->getLocationHero());
+    if(yesOrno) {   
+
         for(auto& villagerMove : villagersMove) {
-            villager.setVillagersPosition(villagerMove, newNameLocationHero);
+            villager.setVillagersPosition(villagerMove, newNameLocationHero, *this);
         }
     }
     this->setHeroesPosition(newNameLocationHero);
@@ -112,7 +111,149 @@ void Heroes::move(Villager& villager,const NameLocation& newNameLocationHero, bo
 
 void Heroes::guide(Villager& villager, const NameVillagers& nameVillager) {  
 
-    villager.setVillagersPosition(nameVillager, this->getLocationHero());
+    villager.setVillagersPosition(nameVillager, this->getLocationHero(), *this);
 
     incrementAction();
+}
+
+
+bool Heroes::cheknumberDistance(const NameLocation& nameLocation) {
+    int numberdistance = this->locationHero.findShortestPath<int>(this->getLocationHero(), nameLocation);
+    return (
+        (numberdistance == 2)
+    );
+}
+
+void Heroes::pickUp(Item& item, const std::vector<NameItem>& nameItem) {
+    
+ if(
+        this->nameLocationHeroes == NameLocation::INN||
+        this->nameLocationHeroes == NameLocation::BARN||
+        this->nameLocationHeroes == NameLocation::INSTITUTE||
+        this->nameLocationHeroes == NameLocation::LABORATORY||
+        this->nameLocationHeroes == NameLocation::MANSION) {
+            this->nameItemPickUpInvisibleMan.emplace_back(nameItem.back(),this->nameLocationHeroes);
+        }
+    for(const auto& name : nameItem) {
+        item.pickUp(this->nameHero, name);
+    }
+
+    incrementAction();
+    
+}
+
+bool Heroes::defieat(Monster& monster, Item& item) {
+    TypeOwnership typeOnserShip;
+    switch (nameHero)
+    {
+    case NameHeroes::ARCHAEOLOGIST:
+        typeOnserShip = TypeOwnership::ARCHAEOLOGIST;
+        break;
+    case NameHeroes::MAYOR:
+        typeOnserShip = TypeOwnership::MAYOR;
+        break;
+    
+    default:
+        throw std::invalid_argument(" ");
+        break;
+    }
+    int chekNumberItem = 0;
+    std::vector<Item> advanceItem;
+    if(monster.getNameM() == NameMonster::DRACULA) {
+
+        for(auto& [name,item] : item.getItemsInGame()) {
+            if(chekNumberItem >= 6) {
+                break;
+            }
+            if(item.getTypeOwnsership() == typeOnserShip) {
+                if(item.getTypeItem() == TypeItem::YELLOW) {
+                    chekNumberItem += item.getPower();
+                    advanceItem.emplace_back(item);
+                }
+            }
+        }
+
+        if(chekNumberItem < 6) {
+            return false;
+        }
+
+        for(auto& item : advanceItem) {
+            item.removeItemInGame(item.getNameItem());
+        }
+
+        monster.removeMonster();
+        return true;
+
+    }
+    else if(monster.getNameM() == NameMonster::INVISIBLE_MAN) {
+        for(auto& [name,item] : item.getItemsInGame()) {
+            if(chekNumberItem >= 9) {
+                break;
+            }
+            if(item.getTypeOwnsership() == typeOnserShip) {
+                if(item.getTypeItem() == TypeItem::RED) {
+                    chekNumberItem += item.getPower();
+                    advanceItem.emplace_back(item);
+                }
+            }
+        }
+
+        if(chekNumberItem < 9) {
+            return false;
+        }
+
+        for(auto& item : advanceItem) {
+            item.removeItemInGame(item.getNameItem());
+        }
+
+        monster.removeMonster();
+        return true;
+
+    }
+    incrementAction();
+
+}
+
+NameHeroes Heroes::getNameHero() const {
+    return this->nameHero;
+}
+
+std::string Heroes::getName() const {
+    switch (nameHero)
+    {
+    case NameHeroes::ARCHAEOLOGIST:
+        return "Archaeologist";
+    case NameHeroes::MAYOR:
+        return "Mayor";
+    default:
+        throw std::invalid_argument("not found Hero");
+    }
+}
+
+void Heroes::setPerkCard(const PerkDeck& newCard) {
+    this->perkCards.emplace_back(newCard);
+}
+
+std::vector<std::pair<NameItem, NameLocation>> Heroes::getNameItemPickUpInvisibleMan() const {
+    return this->nameItemPickUpInvisibleMan;
+}
+
+Archaeologist::Archaeologist(Map& map, PerkDeck& perkDeck) : Heroes(1,NameHeroes::ARCHAEOLOGIST, map, NameLocation::DOCKS, perkDeck) {
+    // بدنه سازنده
+}
+
+Mayor::Mayor(Map& map, PerkDeck& perkDeck) : Heroes(1,NameHeroes::MAYOR, map, NameLocation::THEATRE, perkDeck) {
+    // بدنه سازنده
+}
+
+Scientist::Scientist(Map& map, PerkDeck& perkDeck) : Heroes(4,NameHeroes::SCIENTIST, map, NameLocation::INSTITUTE, perkDeck) {
+    // بدنه سازنده
+}
+
+Courier::Courier(Map& map, PerkDeck& perkDeck) : Heroes(4,NameHeroes::COURIER, map, NameLocation::SHOP, perkDeck) {
+    // بدنه سازنده
+}
+
+const std::vector<PerkDeck>& Heroes::getPerkCards() const {
+    return this->perkCards;
 }
